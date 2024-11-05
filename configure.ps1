e093322b18472c5bb29bc9efcd8c245ad541510d
TrapCyber <assaultchopper@protonmail.com>
2024-11-05 12:20:04 -0600

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -10,7 +10,7 @@ Invoke-WebRequest -Uri https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows
 Expand-Archive -LiteralPath '.\ngrok.zip'
 
 # Set-up and save ngrok authtoken
-./ngrok/ngrok.exe authtoken $env:2YpjnV0MBSGrA970Jux0aShilp5_4hfyHpAmTqEAPVHqaPMHv
+./ngrok/ngrok.exe authtoken $env:NGROK_AUTH_TOKEN
 
 # Enabling RDP Access
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
@@ -19,5 +19,5 @@ Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 # Change password to the one we set-up as RDP_PASSWORD on our repo settings.
-Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "$env:OhShitNotMe1111!" -Force)
+Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "$env:RDP_PASSWORD" -Force)
 Exit
